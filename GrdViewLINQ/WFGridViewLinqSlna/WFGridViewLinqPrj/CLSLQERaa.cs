b912cb203b2dbda11cd5480d3f1a5b1cd5512cce
GooWrites <135832500+GooWrites@users.noreply.github.com>
2023-06-08 22:07:23 +0530

    Add files via upload

---
+++ GrdViewLINQ/WFGridViewLinqSlna/WFGridViewLinqPrj/CLSLQERaa.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WFGridViewLinqPrj;
+// Example for LINQ (ER)
+// Language Intergrated Qurey
+
+namespace WFGridViewLinqPrj
+{
+    class CLSPROPOER
+    {
+        public int _rno;
+        public string _sname;
+        public double _m1 = 0;
+        public double _m2 = 0;
+
+        public int Rno //property name
+        {
+            get
+            {
+                return _rno; // return instance private variable
+            }
+            set
+
+            {
+                _rno = value; //value is reserved word 
+            }
+        }
+        public string SName //property name
+        {
+
+            get
+            {
+                return _sname;
+            }
+            set
+            {
+                _sname = value;
+            }
+        }
+        public double M1
+            {
+            get 
+            {
+                return _m1;
+            }
+            set
+            {
+                _m1 = value;
+            }
+            }
+        public double M2
+        {
+            get
+            {
+                return _m2;
+            }
+            set
+            {
+                _m2 = value;
+            }
+        }
+        public double Total
+
+        {
+            get
+            {
+                return _m1 + _m2;
+
+            }
+        }
+        public double AVG
+        {
+            get
+            {
+                return Total / 2;
+            }
+        }
+        public string Results
+        {
+            get
+            {
+                return _m1 > 34.4 && _m2 > 34.4 ? "Pass" : "Fail";
+            }
+        }
+    }
+
+    class CLSPROPAA
+    {
+        public static void Main ()
+        {
+            CLSPROPOER er = new CLSPROPOER();
+            er.Rno = 1001;
+            er.SName = "Naveen G";
+            er.M1 = 33.3;
+            er.M2 = 89.5;
+
+            Console.WriteLine(er.Rno);
+            Console.WriteLine(er.SName);
+            Console.WriteLine(er.M1);
+            Console.WriteLine(er.M2);
+            Console.WriteLine(er.Total);
+            Console.WriteLine(er.AVG);
+            Console.WriteLine(er.Results);
+            
+        }
+    }
+}
+
+
+/*
+ output 1:
+ 1001
+Gobinath G
+90
+56
+146
+73
+Pass
+Press any key to continue . . .
+
+    output 2:
+1001
+Naveen G
+33.3
+89.5
+122.8
+61.4
+Fail
+Press any key to continue . . .
+
+
+
+ */
---
+++ GrdViewLINQ/WFGridViewLinqSlna/WFGridViewLinqPrj/WFGdVw.aspx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WFGridViewLinqPrj
+{
+    public partial class WFGdVw : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            List<CLSPROPOER> erLst = new List<CLSPROPOER>()
+            {
+                new CLSPROPOER {_rno=1001, _sname="x5", _m1=56.5, _m2=63 },
+                new CLSPROPOER {_rno=1002, _sname="y5", _m1=65.5, _m2=93 },
+                new CLSPROPOER {_rno=1003, _sname="z5", _m1=78, _m2=33 }
+
+            };
+            GridView1.DataSource = erLst;
+            GridView1.DataBind();
+        }
+
+    }
+}
